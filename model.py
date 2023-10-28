c4d314d4ff9ecc2471e9a78e4104442b5b9eab44
Brian Naughton <briannaughton@gmail.com>
2023-10-28 12:07:21 -0700

    crufty edits to gpt

--- model.py
+++ model.py
@@ -10,6 +10,7 @@ https://github.com/huggingface/transformers/blob/main/src/transformers/models/gp
 import math
 import inspect
 from dataclasses import dataclass
+import random
 
 import torch
 import torch.nn as nn
@@ -53,7 +54,10 @@ class CausalSelfAttention(nn.Module):
         B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
 
         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+        # q.shape, k.shape, v.shape = (64 batch_size, 256 block, 384 embed)
+        # x.shape (64 batch, 256 block, 384 embed)
         q, k, v  = self.c_attn(x).split(self.n_embd, dim=2)
+        # q.shape, k.shape, v.shape = (64 batch, 6 heads, 256 block, 64 = 384 embed / 6?)
         k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
         q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
@@ -115,6 +119,74 @@ class GPTConfig:
     dropout: float = 0.0
     bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
 
+# --------------------------------------------
+# TEMPTEMP for debugging
+# change to use the meta in data/ms2/meta.pkl
+import pickle
+with open("data/ms2/meta.pkl", 'rb') as inp:
+    meta = pickle.load(inp)
+stoi, itos = meta['stoi'], meta['itos']
+
+#import pickle, os
+#ckpt_path = os.path.join("out-ms2", 'ckpt.pt')
+#checkpoint = torch.load(ckpt_path)#, map_location=device)
+#meta_path = os.path.join('data', checkpoint['config']['dataset'], 'meta.pkl')
+#with open(meta_path, 'rb') as f:
+#    meta = pickle.load(f)
+### TODO want to make this more general to arbitrary encoder/decoder schemes
+#stoi, itos = meta['stoi'], meta['itos']
+
+def _intensity_penalty(intensity, inflection_point=5.5, steepness=2.0):
+    """
+    Computes a penalty based on intensity values using a sigmoid function.
+
+    Parameters:
+    - intensity (torch.Tensor): The intensity values.
+    - inflection_point (float): The point at which the sigmoid function transitions from low to high.
+    - steepness (float): Controls the steepness of the sigmoid curve.
+
+    Returns:
+    - torch.Tensor: The penalty values corresponding to the input intensity values.
+    """
+    # Adjust the intensity values based on the inflection point and steepness
+    adjusted_intensity = (intensity - inflection_point) * steepness
+
+    # Compute the sigmoid of the adjusted intensity
+    penalty = 1 / (1 + torch.exp(-adjusted_intensity))
+
+    return penalty
+
+
+def _custom_loss(logits2, targets):
+    """
+    Computes the cross-entropy loss between the histogram of targets
+    and the mean softmax probabilities of logits2.
+
+    Parameters:
+        logits2 (Tensor): The predicted scores.
+        targets (Tensor): The true labels.
+
+    Returns:
+        Tensor: The cross-entropy loss.
+    """
+    # Ensure targets is a 1D tensor
+    targets_1d = targets.view(-1)
+
+    # Compute histogram of targets
+    target_hist = torch.bincount(targets_1d, minlength=logits2.size(-1)).float()
+    target_hist = target_hist / target_hist.sum()  # Normalize to create a distribution
+
+    # Compute mean softmax probabilities of logits2
+    softmax_logits_mean = F.softmax(logits2, dim=-1).mean(dim=1)
+
+    # Compute cross-entropy for each batch
+    loss = -(target_hist.unsqueeze(0) * torch.log(softmax_logits_mean + 1e-12))
+
+    # Get the mean for each batch, then the mean across batches
+    return loss.mean(dim=1).mean(dim=0)
+
+
+
 class GPT(nn.Module):
 
     def __init__(self, config):
@@ -124,18 +196,42 @@ class GPT(nn.Module):
         self.config = config
 
         self.transformer = nn.ModuleDict(dict(
-            wte = nn.Embedding(config.vocab_size, config.n_embd),
-            wpe = nn.Embedding(config.block_size, config.n_embd),
+            wte = nn.Embedding(config.vocab_size, config.n_embd, padding_idx=0),
+            wpe = nn.Embedding(config.block_size, config.n_embd, padding_idx=0),
             drop = nn.Dropout(config.dropout),
             h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
             ln_f = LayerNorm(config.n_embd, bias=config.bias),
         ))
+
+        # attempt to add encoder and decoder
+        # should encoder and decoder have different n_layer?
+        # currently they are the same
+        self.BOTTLENECK_SIZE = 64
+        self.NUM_INTENSITIES = 10
+        self.encoder = nn.ModuleDict(dict(
+            wte = nn.Embedding(config.vocab_size, config.n_embd, padding_idx=0),
+            wpe = nn.Embedding(config.block_size, config.n_embd, padding_idx=0),
+            wie = nn.Embedding(self.NUM_INTENSITIES, config.n_embd, padding_idx=0),
+            drop = nn.Dropout(config.dropout),
+            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+            ln_f = LayerNorm(config.n_embd, bias=config.bias),
+            bottleneck = nn.Linear(config.block_size * config.n_embd, self.BOTTLENECK_SIZE)
+        ))
+        self.decoder = nn.ModuleDict(dict(
+            unbottleneck = nn.Linear(self.BOTTLENECK_SIZE, config.block_size * config.n_embd),
+            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+            drop = nn.Dropout(config.dropout),
+            ln_f = LayerNorm(config.n_embd, bias=config.bias),
+        ))
+
+        # why is lm_head outside the transformer?
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
         # with weight tying when using torch.compile() some warnings get generated:
         # "UserWarning: functional_call was passed multiple values for tied weights.
         # This behavior is deprecated and will be an error in future versions"
         # not 100% sure what this is, so far seems to be harmless. TODO investigate
         self.transformer.wte.weight = self.lm_head.weight # https://paperswithcode.com/method/weight-tying
+        self.encoder.wte.weight = self.lm_head.weight # https://paperswithcode.com/method/weight-tying
 
         # init all weights
         self.apply(self._init_weights)
@@ -167,23 +263,103 @@ class GPT(nn.Module):
         elif isinstance(module, nn.Embedding):
             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
+
+
     def forward(self, idx, targets=None):
+        # when training:
+        # idx torch.Size([64 batch_size, 256 block_size]) tensor([[51, 43,  1,  ..., 53, 44,  1],
+        # when "sampling", it grows one at a time:
+        # idx torch.Size([1, 1]) tensor([[0]], device='cuda:0')
+        # idx torch.Size([1, 2]) tensor([[ 0, 15]], device='cuda:0')
         device = idx.device
         b, t = idx.size()
         assert t <= self.config.block_size, f"Cannot forward sequence of length {t}, block size is only {self.config.block_size}"
         pos = torch.arange(0, t, dtype=torch.long, device=device) # shape (t)
 
+        ins = torch.randint(0, self.NUM_INTENSITIES, (t,), dtype=torch.long, device=device) # shape (t)
+
+        # --------------------------------------------
+        # old Transformer
         # forward the GPT model itself
+        # train: tok_emb / x shape  (64 batch_size, 256 block_size, 384 embed)
+        # sample: tok_emb / x shape (1 batch_size, 1,2,3 block_size, 384 embed)
+        '''
         tok_emb = self.transformer.wte(idx) # token embeddings of shape (b, t, n_embd)
         pos_emb = self.transformer.wpe(pos) # position embeddings of shape (t, n_embd)
-        x = self.transformer.drop(tok_emb + pos_emb)
+
+        x = self.transformer.drop(tok_emb)# + pos_emb)
         for block in self.transformer.h:
             x = block(x)
+        # x doe not change shape here
         x = self.transformer.ln_f(x)
+        '''
+
+        # --------------------------------------------
+        # TEST encoder
+        # what i have: (32 batch_size, 16 block_size, 12 bottleneck)
+        # what i want: (32 batch_size, 12 bottleneck)
+        tok_emb = self.encoder.wte(idx) # token embeddings of shape (b, t, n_embd)
+        pos_emb = self.encoder.wpe(pos) # position embeddings of shape (t, n_embd)
+        ins_emb = self.encoder.wie(ins) # position embeddings of shape (t, n_embd)
+
+        z = self.encoder.drop(tok_emb + pos_emb)
+        for block in self.decoder.h:
+            z = block(z)
+        # z does not change shape here
+        z = self.encoder.ln_f(z)
+        z_flat = z.view(z.size(0), -1)
+
+        # flat representation of one block
+        z_bottleneck = self.encoder.bottleneck(z_flat)
+
+        # TEST decoder
+        # z_flat = (32 batch_size, 128 flattened)
+        z_flat = self.decoder.unbottleneck(z_bottleneck)
+        z = z_flat.view(z_flat.size(0), self.config.block_size, self.config.n_embd)
+        for block in self.encoder.h:
+            z = block(z)
+        z = self.decoder.drop(z) # test adding dropout to decoder too
+        z = self.decoder.ln_f(z)
+        # logits = (32 batch_size, 16 block_size, 22 vocab_size)
+        # logits.view(-1, logits.size(-1)).shape = (512, 22), just flatten batches for cross_entropy
+        logits2 = self.lm_head(z)
 
         if targets is not None:
+            # logits = (batch_size, block_size, vocab_size)
+            #print(logits2[0], torch.argmax(logits2[0][0]))
+            loss2 = F.cross_entropy(logits2.view(-1, logits2.size(-1)), targets.view(-1), ignore_index=-1)
+
+            # Test alternate loss
+            _, predicted_tokens = torch.max(logits2, dim=-1)
+            #tmp = logits2[:,0,:].clone()
+            #logits2[:,0,:] = logits2[:,2,:]
+            #logits2[:,2,:] = tmp
+            alt_loss2 = _custom_loss(logits2, targets)
+
+        #
+        # Print out status periodically (without iter!)
+        #
+        if random.random() < .0005 and round(float(loss2), 3) != round(float(alt_loss2),3):
+            most_likely_tokens = torch.argmax(logits2, dim=-1)
+            print("TTT1", ' '.join([itos[int(t)] for t in idx[0]]))
+            print("TTT2", ' '.join([itos[int(t)] for t in most_likely_tokens[0]]))
+            print("TTT3", loss2, alt_loss2)
+
+        if random.random() < 0.005:
+            open("out.loss_vs_alt.txt", 'a').write(f"{loss2}\t{alt_loss2}\n")
+            print("LLL", loss2, alt_loss2)
+
+        return logits2, loss2
+
+        # --------------------------------------------
+
+        # targets shape = (64 batch_size, 256 block_size)
+        if targets is not None:
             # if we are given some desired targets also calculate the loss
+            # logits shape of (64 batch_size, 256 block_size, 65 vocab_size)
             logits = self.lm_head(x)
+            # logits.shape = (64 batch, 256 block, 65 vocab_size)
+            # logits.view(-1, logits.size(-1)).shape = (16384 batch*block, 65 vocab_size)
             loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
         else:
             # inference-time mini-optimization: only forward the lm_head on the very last position
@@ -327,4 +503,7 @@ class GPT(nn.Module):
             # append sampled index to the running sequence and continue
             idx = torch.cat((idx, idx_next), dim=1)
 
+            END_OF_SENTENCE_IDX = 2 # "}" for ms2
+            if idx_next == END_OF_SENTENCE_IDX:
+                break
         return idx
