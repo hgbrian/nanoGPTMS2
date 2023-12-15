4d66b900849e2d55e8bf447e7e91c34d6fc43657
Brian Naughton <briannaughton@gmail.com>
2023-12-15 12:34:56 -0800

    wandb names

--- config/train_ms2.py
+++ config/train_ms2.py
@@ -11,21 +11,23 @@ always_save_checkpoint = False
 
 wandb_log = True # override via command line if you like
 wandb_project = 'ms2'
-wandb_run_name = 'ms2-1'
 
 dataset = 'ms2'
 gradient_accumulation_steps = 1
-batch_size = 32
+batch_size = 24
 block_size = 128 # context of up to 256 previous characters
 
 # baby GPT model :)
-n_layer = 2
-n_head = 2
-n_embd = 128
+n_layer = 8
+n_head = 8
+n_embd = 1024
 dropout = 0.2
+n_bottleneck = 64
+
+wandb_run_name = f"ms2-{n_bottleneck}_{n_layer}_{n_layer}_{n_embd}"
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 20_000
+max_iters = 100_000_000 # just interrupt
 lr_decay_iters = max_iters # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
